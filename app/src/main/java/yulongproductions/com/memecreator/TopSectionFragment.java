61cfa12a017ce2d43aa2b1c37c912bcfaf3145e7
Yulong Tan <swifties@uw.edu>
2015-11-16 18:38:57 -0800

    added nullable annotation

--- app/src/main/java/yulongproductions/com/memecreator/TopSectionFragment.java
+++ app/src/main/java/yulongproductions/com/memecreator/TopSectionFragment.java
@@ -1,6 +1,7 @@
 package yulongproductions.com.memecreator;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +10,8 @@ import android.view.ViewGroup;
 public class TopSectionFragment extends Fragment {
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+							 @Nullable Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.top_section_fragment, container, false);
 		return view;
 	}
