5920dadc29a36cef90438fa411a893453128c866
Yulong Tan <swifties@uw.edu>
2015-11-16 19:03:52 -0800

    Created the fragment for the picture

---
+++ app/src/main/java/yulongproductions/com/memecreator/BottomPictureFragment.java
@@ -0,0 +1,18 @@
+package yulongproductions.com.memecreator;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BottomPictureFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.bottom_image_fragment, container, false);
+        return view;
+    }
+}
