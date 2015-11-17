ff943957aba6c641cc10673f82dee50a2e8a4075
Yulong Tan <swifties@uw.edu>
2015-11-16 19:13:09 -0800

    created the fields for the widgets and set the button to change the meme text

--- app/src/main/java/yulongproductions/com/memecreator/MainActivity.java
+++ app/src/main/java/yulongproductions/com/memecreator/MainActivity.java
@@ -4,14 +4,40 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+    private EditText memeLine1;
+    private EditText memeLine2;
+    private TextView topText;
+    private TextView bottomText;
+    private Button memeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.memeLine1 = (EditText) findViewById(R.id.memeLine1);
+        this.memeLine2 = (EditText) findViewById(R.id.memeLine2);
+        this.topText = (TextView) findViewById(R.id.topText);
+        this.bottomText = (TextView) findViewById(R.id.bottomText);
+        this.memeButton = (Button) findViewById(R.id.memeButton);
+
+        this.memeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                topText.setText(convert(memeLine1));
+                bottomText.setText(convert(memeLine2));
+            }
+        });
+    }
+
+    private String convert(EditText editText) {
+        return editText.getText().toString();
     }
 
     @Override
