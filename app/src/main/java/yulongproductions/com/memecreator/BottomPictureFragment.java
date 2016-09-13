ed07c9c54bccad4c8a6a2637f4a8defb965cc629
Yulong Tan <swifties@uw.edu>
2016-09-13 13:41:12 -0700

    add setMemeText

--- app/src/main/java/yulongproductions/com/memecreator/BottomPictureFragment.java
+++ app/src/main/java/yulongproductions/com/memecreator/BottomPictureFragment.java
@@ -6,13 +6,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class BottomPictureFragment extends Fragment {
+    private TextView mTopText;
+    private TextView mBottomText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.bottom_image_fragment, container, false);
+
+        mTopText = (TextView) view.findViewById(R.id.topText);
+        mBottomText = (TextView) view.findViewById(R.id.bottomText);
+
         return view;
+    }
+
+    public void setMemeText(String top, String bottom) {
+        mTopText.setText(top);
+        mBottomText.setText(bottom);
     }
 }
